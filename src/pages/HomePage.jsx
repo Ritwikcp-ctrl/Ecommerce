5b83937218fb8867464ea58ceafee70ca3b6346c
Ritwikcp-ctrl <su30rai213@gmai.com>
2025-11-08 18:39:26 +0530

    Fetch data using Axios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
   import './header.css';
+  import axios from 'axios';
   import './HomePage.css';
   import {products} from './Data/products.js';
   
   export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-       return response.json();
+     console.log(response.data);
        
-      }).then((data)=>{
-          console.log(data);
     });
   
   
